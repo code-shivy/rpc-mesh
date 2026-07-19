2e4f15e4304a6b3a4a1dc721f827c0b3a2995cbd
Shubham-astro <shubham.singh@astronomer.io>
2026-07-19 17:05:31 +0530

    http server

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+import  (
+	"log"
+	"net/http"
+)
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+		})
+
+	log.Println("rpc-mesh listening on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
